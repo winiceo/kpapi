f798261c346086a10d3806efbdb4b104c7ccb6e9
赵少辉 <zhaoshaohui@pmkoo.com>
2016-06-01 11:55:58 +0800

    afd

---
+++ api/controllers/PollController.js
@@ -0,0 +1,11 @@
+/**
+ * PollController
+ *
+ * @description :: Server-side logic for managing polls
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
